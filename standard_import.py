b235b0f11bfd54e3a481c25ed77d9963d2b7b772
LexBrown <44871845+lexbrown@users.noreply.github.com>
2020-02-19 16:43:41 +0300

    Create standard_import.py

---
+++ standard_import.py
@@ -0,0 +1,8 @@
+import os, sys
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+%matplotlib inline 
+import seaborn as sns
+import warnings
+warnings.filterwarnings('ignore')
