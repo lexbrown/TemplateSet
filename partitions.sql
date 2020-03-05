a3cab8239d97260dab3dc72a8a67c3dd2631aa41
LexBrown <44871845+lexbrown@users.noreply.github.com>
2020-03-05 15:15:37 +0300

    Update partitions.sql

--- partitions.sql
+++ partitions.sql
@@ -19,4 +19,5 @@
     
 -- Работа со временем
 select from_unixtime(unix_timestamp('2015-12-12 16:15:17')+3600);
-select unix_timestamp('2015-04-09 03:04:26') from dual;
+select unix_timestamp('2015-04-09 03:04:26') from dual; -- results in "1428566666"
+select from_unixtime(1428566666) from dual; -- results in "2015-04-09 03:04:26"
